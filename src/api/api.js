4cd339e41d7caed902702b998c1c8c5638455396
JTSG1 <james@tesh.digital>
2024-09-22 22:09:50 +0100

    feat: readded prefix to env var

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with the base URL from environment variable
 const api = axios.create({
-    baseURL: process.env.REACT_APP_FINANCE_API_HOST,
+    baseURL: process.env.REACT_APP_FINANCE_APP_API_HOST,
     timeout: 10000, // 10 seconds
     headers: {
         'Content-Type': 'application/json',
